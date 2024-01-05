3f3ad53dd76111eb06ce981ab1ee2ad83614b0cc
Admin <Admin@DESKTOP-BK8R38T>
2024-01-05 12:02:51 +0530

    Eng2 FirstCommit

---
+++ JenkinsDemo/src/test/java/testCase/Tc2.java
@@ -0,0 +1,14 @@
+package testCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2 {
+	
+	@Test
+	public void tc2()
+	{
+		Reporter.log("Tc2 Ruuning",true);
+	}
+
+}
