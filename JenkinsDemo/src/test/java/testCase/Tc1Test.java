4d0850c4dec9b13f333e0d68384ed1dd51218d58
Admin <Admin@DESKTOP-BK8R38T>
2024-01-05 11:53:47 +0530

    First Commit By Eng1

---
+++ JenkinsDemo/src/test/java/testCase/Tc1Test.java
@@ -0,0 +1,15 @@
+package testCase;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1Test {
+	
+	@Test
+	public void m1()
+	{
+		Reporter.log("Test Case 1",true);
+	}
+
+}
+ 
